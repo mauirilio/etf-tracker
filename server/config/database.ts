6153597d6c4e865789bb76373d70d6e769bcc2a5
Maurilio Carvalho Almeida <2103458@ifms.edu.br>
2025-12-26 15:44:37 -0400

    Fix: Configuração SSL do Banco para TypeScript

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,24 +1,29 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Options } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const dbOptions: Options = {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: false,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+};
+
+if (process.env.DB_HOST && process.env.DB_HOST !== 'localhost') {
+  dbOptions.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  };
+}
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'etfs-2025',
   process.env.DB_USER || 'postgres',
   process.env.DB_PASS || 'postgres',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    logging: false,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    dialectOptions: {
-      ssl: process.env.DB_HOST && process.env.DB_HOST !== 'localhost' ? {
-        require: true,
-        rejectUnauthorized: false // Necessário para Neon e muitos provedores cloud
-      } : false
-    }
-  }
+  dbOptions
 );
 
 export default sequelize;
